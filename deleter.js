be2efedb736ec9f263cbcaa48c512354cd48835d
ShiningLight <aizy.fpg@gmail.com>
2021-04-15 19:02:08 +0300

    feat: null and undefined handlers added

--- deleter.js
+++ deleter.js
@@ -5,38 +5,51 @@ function deleteAllEvenLengthElems(array) {
   array.forEach((el) => {
     if (
       !JSON.stringify(collection).includes(JSON.stringify(el)) ||
-      ((Number.isFinite(el) || Number.isNaN(el)) && !collection.includes(el))
+      ((Number.isFinite(el) ||
+        Number.isNaN(el) ||
+        el === null ||
+        el === undefined) &&
+        !collection.includes(el))
     ) {
       collection.push(el);
     }
   });
 
+  console.log(collection);
+
   collection.forEach((el) => {
     let filtred = array.filter((i) => {
+      if (i === null && el === null) {
+        console.log(el, i, el === null && i === null);
+        return true;
+      }
+      if (infinityCheck(i) && infinityCheck(el)) {
+        return true;
+      }
       if (Number.isNaN(i) && Number.isNaN(el)) return true;
-      if (typeof i === "object")
+      if (typeof i === "object" && !(i === null) && !(el === null))
         return JSON.stringify(i) === JSON.stringify(el);
       return i === el;
     }).length;
     countedCollection.set(el, filtred);
   });
 
+  console.log(countedCollection);
+
   filtredArray = [].concat(array);
 
   for (let [key, value] of countedCollection) {
     if (!(value & 1)) {
       filtredArray = filtredArray.filter((i) => {
-        if (typeof i === "object" && typeof key === "object")
+        if (
+          typeof i === "object" &&
+          typeof key === "object" &&
+          !(i === null) &&
+          !(key === null)
+        )
           return JSON.stringify(i) === JSON.stringify(key);
 
-        if (
-          !Number.isFinite(key) &&
-          !Number.isFinite(i) &&
-          typeof key === "number" &&
-          typeof i === "number" &&
-          key == "Infinity" &&
-          i == "Infinity"
-        ) {
+        if (infinityCheck(key) && infinityCheck(i)) {
           return false;
         }
 
@@ -47,4 +60,12 @@ function deleteAllEvenLengthElems(array) {
     }
   }
   return filtredArray;
+}
+
+function infinityCheck(element) {
+  return (
+    !Number.isFinite(element) &&
+    typeof element === "number" &&
+    element == "Infinity"
+  );
 }
